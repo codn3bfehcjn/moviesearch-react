f9cdd89035b0540e569958f2d918bb077392aff5
vishwam thakur <159876492+codn3bfehcjn@users.noreply.github.com>
2024-10-11 10:03:36 +0530

    Update fetchapi.jsx

--- something/src/components/fetchapi.jsx
+++ something/src/components/fetchapi.jsx
@@ -26,6 +26,6 @@ function FetchApi({ moviename, fn }) {
 //   else{
 //     fn(null)  // if moviename is empty pass null to the home component so the info part is cleared automatically no need to refresh page
 //   }
-// }, [moviename]); // dependency on fn will call api infinitely. because setdata is called everytime the data is send by fn causing home to re-render also fn is also reinitialised on ecah re render
+// }, [moviename]); // dependency on (fn) will call api infinitely. because setdata is called everytime the data is send by fn causing home to re-render also fn is also reinitialised on ecah re render
 
 export default FetchApi;
